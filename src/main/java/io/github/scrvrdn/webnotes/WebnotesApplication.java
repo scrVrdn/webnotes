64087d388dc457e24625edab4b42d474ddc4e11b
scrVrdn <240490351+scrVrdn@users.noreply.github.com>
2026-03-05 12:56:13 +0100

    Initial commit

---
+++ src/main/java/io/github/scrvrdn/webnotes/WebnotesApplication.java
@@ -0,0 +1,13 @@
+package io.github.scrvrdn.webnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebnotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebnotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/scrvrdn/webnotes/WebnotesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.scrvrdn.webnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebnotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
